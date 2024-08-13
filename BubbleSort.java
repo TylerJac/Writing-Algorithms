19a0de19f87ec300f068dee7512fc67b9e548783
TylerJac <sans.gaming235@gmail.com>
2024-08-13 10:59:45 -0400

    bubblesort

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = 5;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 3, 9, 7};
+        bubbleSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
